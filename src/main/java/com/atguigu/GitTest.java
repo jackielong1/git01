7f8660f89e74a146159494b7ee132a980a52df9b
jackie <jackie@qq.com>
2022-08-11 21:06:02 +0800

    first commit jackie

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author 陈龙
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试成功");
+    }
+}
